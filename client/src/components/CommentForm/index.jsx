25e2415428b9e4d137bd5c9d9a79a73c596ec9c1
kateleighb3 <kbaldw3@gmail.com>
2023-09-27 13:20:23 -0400

    heroku

--- client/src/components/CommentForm/index.jsx
+++ client/src/components/CommentForm/index.jsx
@@ -6,7 +6,6 @@ import { ADD_COMMENT } from '../../utils/mutations';
 
 import Auth from '../../utils/auth';
 
-//
 
 const CommentForm = ({ thoughtId }) => {
   const [commentText, setCommentText] = useState('');
