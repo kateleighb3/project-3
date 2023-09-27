3a3d638f3aad225bd9d1e8e814636252639f1fa0
kateleighb3 <kbaldw3@gmail.com>
2023-09-27 12:42:05 -0400

    deploy heroku

--- client/src/components/CommentForm/index.jsx
+++ client/src/components/CommentForm/index.jsx
@@ -6,6 +6,10 @@ import { ADD_COMMENT } from '../../utils/mutations';
 
 import Auth from '../../utils/auth';
 
+//
+
+
+
 const CommentForm = ({ thoughtId }) => {
   const [commentText, setCommentText] = useState('');
   const [characterCount, setCharacterCount] = useState(0);
